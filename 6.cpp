3de82379631bd7e752fa5ebc93c15ddd379c96be
joshuajoy73 <131249710+joshuajoy73@users.noreply.github.com>
2024-01-04 11:32:06 +0530

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,17 @@
+
+void preorder(TreeNode* root,vector<int>& ans){
+if(root==nullptr){
+    return ;
+}
+ans.push_back(root->val);
+ preorder(root->left,ans);
+  preorder(root->right,ans);
+}
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int > ans;
+        preorder(root,ans);
+        return ans ;   
+    }
+};
